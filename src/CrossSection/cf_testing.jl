6fee2df1fe3035734ed1180c0085ab8fa2cfad70
Christian Frankenberg <fronge@gmail.com>
2020-07-23 15:33:40 -0700

    added Voigt lineshape

--- src/CrossSection/cf_testing.jl
+++ src/CrossSection/cf_testing.jl
@@ -2,6 +2,6 @@ include("./CrossSection.jl")
 using .CrossSection.HITRAN
 using .CrossSection.VoigtShape
 
-df = readHITRAN("/net/fluo/data1/projects/RTM/Mie/hitran/par/02_hit04.par", 2, 1, 0, 30000)
+df = readHITRAN("/Users/cfranken/Desktop/02_hit04.par", 2, 1, 0, 30000)
 
 computeVoigt(df, collect(1:20000), 950, 298, 1, 100)
