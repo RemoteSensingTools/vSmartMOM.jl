25a86041b29b8e3657fb5fdb9c01ef7934d66f08
RupeshJey <rupeshjey@gmail.com>
2020-08-20 10:36:48 -0700

    Initial docs full commit

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,26 @@
 using Documenter, RadiativeTransfer, RadiativeTransfer.CrossSection
 
-makedocs(sitename="Radiative Transfer")
+# generated_dir = joinpath(@__DIR__, "src", "generated") # generated files directory
+# rm(generated_dir, force = true, recursive = true)
+# mkpath(generated_dir)
+
+pages = Any[
+    "Home"           => "index.md",
+    "CrossSection"   => "pages/CrossSection.md"
+    ]
+
+# mathengine = MathJax(Dict(
+#     :TeX => Dict(
+#         :equationNumbers => Dict(:autoNumber => "AMS"),
+#         :Macros => Dict(),
+#     ),
+# ))
+
+# format = Documenter.HTML(
+#     prettyurls = get(ENV, "CI", nothing) == "true",
+#     mathengine = mathengine,
+#     collapselevel = 1,
+# )
+
+makedocs(sitename="Radiative Transfer", 
+         pages=pages)
