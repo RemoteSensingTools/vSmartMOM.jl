ab49cc92d6aaa998478b133f560123f5dbe5cef9
cfranken <cfranken@tofu.gps.caltech.edu>
2020-09-19 17:07:12 -0700

    Speed up computeB

--- src/PhaseFunction/compute_Sl.jl
+++ src/PhaseFunction/compute_Sl.jl
@@ -53,7 +53,7 @@ function goCUDA!(mat_anamC, mat_bnbmC,mat_anbmC,mat_bnamC)
     return nothing
 end
 
-function compute_Sl(l, ν₁, ν₂, ν₂_positive_flag, k, N_max, an, bn, mat_anam, mat_bnbm, mat_anbm, mat_bnam, wigner_A, wigner_B, w)
+function compute_Sl(l, ν₁, ν₂, ν₂_positive_flag, k, N_max, an, bn, mat_anam, mat_bnbm, mat_anbm, mat_bnam, wigner_A, wigner_B,an_m_bn,an_p_bn, w)
 
     @assert ((ν₁, ν₂) == (2, -2) || (ν₁, ν₂) == (2, 2) || (ν₁, ν₂) == (0, 0) || (ν₁, ν₂) == (0, 2)) "Not a valid input to calculate Sl"
     @assert ((ν₂ == 2 && ν₂_positive_flag == true) || (ν₂ == -2 && ν₂_positive_flag == false) || ν₂ == 0) "Sign flag doesn't match ν₂"
@@ -61,21 +61,21 @@ function compute_Sl(l, ν₁, ν₂, ν₂_positive_flag, k, N_max, an, bn, mat_
     ll = l-1
     coef = (2ll+1)*π / k^2
 
-    first_term = 0
-    second_term = 0
+    first_term = 0.0
+    second_term = 0.0
 
     if (ν₁, ν₂) == (0, 2)
 
-        for n = 1:N_max
+        @inbounds for n = 1:N_max
             m_star = max(ll-n,n+1)
             m_max = min(ll+n,N_max)
 
-            for m = m_star:m_max
+            @inbounds for m = m_star:m_max
 
-                anam,bnbm,anbm,bnam = (mat_anam[m,n], mat_bnbm[m,n], mat_anbm[m,n], mat_bnam[m,n])
-                aman,bmbn,ambn,bman = get_abnabm(an,bn,m,n,w)
-
-                avg = (exp_m1(ll + n + m)) * (anam + bnam - anbm - bnbm) + (aman - ambn + bman - bmbn)
+                #anam,bnbm,anbm,bnam = (mat_anam[m,n], mat_bnbm[m,n], mat_anbm[m,n], mat_bnam[m,n])
+                #aman,bmbn,ambn,bman = get_abnabm(an,bn,m,n,w)
+                #@show anam,aman
+                avg = (exp_m1(ll + n + m)) * (mat_anam[m,n] + mat_bnam[m,n] - mat_anbm[m,n] - mat_bnbm[m,n]) + (mat_anam[m,n]' - mat_bnam[m,n]' + mat_anbm[m,n]' - mat_bnbm[m,n]')
                 first_term += avg * (2n+1) * (2m+1) * wigner_A[m, n, l] * wigner_B[m, n, l]
             end
 
@@ -86,12 +86,13 @@ function compute_Sl(l, ν₁, ν₂, ν₂_positive_flag, k, N_max, an, bn, mat_
 
     else
         wig_lnm = ν₁ == 0 ? wigner_A : wigner_B
+        
 
-        for n = 1:N_max
+        @inbounds for n = 1:N_max
             m_star = max(ll-n,n+1)
             m_max = min(ll+n,N_max)
             
-            for m = m_star:m_max
+            @inbounds for m = m_star:m_max
                 anam,bnbm,anbm,bnam = (mat_anam[m,n], mat_bnbm[m,n], mat_anbm[m,n], mat_bnam[m,n]) # get_abnabm(an,bn,n,m,w)
                 real_avg = ν₂_positive_flag ? real(anam+anbm+bnam+bnbm) : real(anam-anbm-bnam+bnbm);
                 coef_lnm2 = 2 * (2m + 1)  * (2n + 1) * (ν₂_positive_flag ? 1 : (exp_m1(ll + n + m)))
@@ -99,20 +100,13 @@ function compute_Sl(l, ν₁, ν₂, ν₂_positive_flag, k, N_max, an, bn, mat_
             end
 
             coef_lnn2 = (2n + 1)^2 * (ν₂_positive_flag ? 1 : (exp_m1(ll)))
-
-            for i = 1:size(an,1)
-                if (ν₂_positive_flag)
-                    second_term += w[i]' * (abs2(an[i,n] + bn[i,n])) * (wig_lnm[n, n, l])^2 * coef_lnn2
-                else 
-                    second_term += w[i]' * (abs2(an[i,n] - bn[i,n])) * (wig_lnm[n, n, l])^2 * coef_lnn2
-                end
+            
+            if (ν₂_positive_flag)
+                second_term += an_p_bn[n] * (wig_lnm[n, n, l])^2 * coef_lnn2
+            else 
+                second_term += an_m_bn[n] * (wig_lnm[n, n, l])^2 * coef_lnn2
             end
 
-            # if (ν₂_positive_flag)
-            #     second_term += w' *(abs2.(an[n,:] .+ bn[n,:])) * (wig_lnm[n, n, l])^2 * coef_lnn2
-            # else 
-            #     second_term += w' *(abs2.(an[n,:] .- bn[n,:])) * (wig_lnm[n, n, l])^2 * coef_lnn2
-            # end
         end
     end
 
