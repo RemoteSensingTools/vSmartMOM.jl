c9a50aaf4c975fcff053595f33e8a9a98fd0e72b
Suniti Sanghavi <sanghavi@Christians-iMac.local>
2020-11-01 18:54:17 -0800

    Add basic prototype code for vSmartMOM RTM

---
+++ src/RTM/rt_elemental.jl
@@ -0,0 +1,54 @@
+"Elemental single-scattering layer"
+function rt_elemental!(mo::Stokes_IQUV, dτ, ϖ, bb, m, n, ndoubl, scatter)
+    # ToDo: Main output is r_elt_pm, r_elt_mp, t_elt_mm, t_elt_pp (can be renamed to t⁺⁺, etc)
+    # Need to check with paper nomenclature. This is basically eqs. 19-20 in vSmartMOM
+
+    #dτ: optical depth of elemental layer
+    #ϖ: single scattering albedo of elemental layer
+    #bb: thermal source function at the upper boundary of the elemental layer
+    #m: fourier moment
+    #n: layer of which this is an elemental
+    #ndoubl: number of doubling computations needed to progress from the elemental layer to the full homogeneous layer n
+    #scatter: flag indicating scattering
+
+    r_elt_mp = zeros(Nquad4,Nquad4)
+    t_elt_pp = zeros(Nquad4,Nquad4)
+    if scatter
+        #TODO: import vector containing quadrature cosines qp_μ of length Nquad4
+        #TODO: import vector containing quadrature weights wt_μ of length Nquad4
+        #TODO: construct composite, post-truncation dτ=τ/2^{ndoubl} , ϖ, p⁺⁺, p⁻⁺ matrices and import them here
+        if m==0
+            wct=0.50 * ϖ * wt_μ 
+        else    
+            wct=0.25 * ϖ * wt_μ
+        end
+        r_elt_mp = diag(1./qp_μ) * p⁻⁺ * diag(wct) * dτ
+        t_elt_pp = I - (diag(1./qp_μ) * (I - p⁺⁺ * diag(wct))) * dτ)
+        if ndoubl<1
+            for iμ in 1:Nquad4; jμ in 1:Nquad4
+                # That "4" and Nquad4 needs to be dynamic, coming from the PolType struct.
+                i=mod(iμ-1,4)
+                j=mod(jμ-1,4)
+                if ((i<=1)&(j<=1)) | ((i>=2)&(j>=2))
+                    r_elt_pm[iμ,jμ] = r_elt_mp[iμ,jμ]
+                    t_elt_mm[iμ,jμ] = t_elt_pp[iμ,jμ]
+                else
+                    r_elt_pm[iμ,jμ] = - r_elt_mp[iμ,jμ]
+                    t_elt_mm[iμ,jμ] = - t_elt_pp[iμ,jμ]
+                end
+            end 
+        else
+            #For doubling, transform R->DR, where D = diag{1,1,-1,-1}
+            for iμ in 1:Nquad4;
+                i=mod(iμ-1,4)    
+                if (i>=2)
+                    r_elt_pm[iμ,:] = - r_elt_mp[iμ,:]
+                end
+            end 
+        end
+    else
+        t_elt_pp = diag{exp(τ./qp_μ)}
+        t_elt_mm = diag{exp(τ./qp_μ)}
+    end 
+    return nothing
+end
---
+++ src/RTM/rt_interaction.jl
@@ -0,0 +1,52 @@
+"Simulates the full atmosphere from n distinct homogeneous layers"
+function rt_interaction!(mo::Stokes_IQUV, kn, n, m)
+    # ToDo: Important output from this routine is R_mp, R_pm, T_pp, T_mm (can be renamed to 𝐓⁻⁻, etc later)
+    # Need to check with paper nomenclature. This is basically eqs. 23-28 in vSmartMOM)
+
+    # kn = 1: no scattering in either the added layer or composite layer.
+    # kn = 2: composite layer has no scattering but added layer does.
+    # kn = 3: composite layer has scattering but added layer does not.
+    # kn = 4: both composite layer and added layer have scattering.
+    if kn==1
+        # No scattering in either the added layer or the composite layer.
+        for iμ in 1:Nquad4
+            T_mm[iμ, iμ] = t_elt_mm[iμ, iμ]*T_mm[iμ, iμ]
+            T_pp[iμ, iμ] = t_elt_pp[iμ, iμ]*T_pp[iμ, iμ]
+        end
+        return nothing
+    elseif kn==2
+        # No scattering in inhomogeneous composite layer.
+        # scattering in homogeneous layer which is added 
+        # to the bottom of the composite layer.
+        # Produces a new, scattering composite layer.
+        M1=T_mm
+        M2=T_pp
+        R_mp = M1 * r_elt_mp * M2
+        R_pm = r_elt_pm
+        T_pp = t_elt_pp * M2
+        T_mm = M1 * t_elt_mm
+        return nothing
+    elseif kn==3
+        # Scattering in inhomogeneous composite layer.
+        # no scattering in homogeneous layer which is 
+        # added to the bottom of the composite layer.
+        # Produces a new, scattering composite layer.
+        T_pp = t_elt_pp * T_pp
+        T_mm = T_mm * t_elt_mm
+        R_pm = t_elt_pp * R_pm * t_elt_mm
+        return nothing
+    elseif kn==4
+        # Scattering in inhomogeneous composite layer.
+        # scattering in homogeneous layer which is 
+        # added to the bottom of the composite layer.
+        # Produces a new, scattering composite layer.
+        M1 = inv(I - R_pm * r_elt_mp)
+        t_R_mp = R_mp + T_mm * r_elt_mp * M1 * T_pp
+        t_T_pp = t_elt_pp * M1 * T_pp
+        #repeating for mirror-reflected directions
+        M1 = inv(I - r_elt_mp * R_pm)
+        t_R_pm = r_elt_pm + t_elt_pp * R_pm * M1 * t_elt_mm
+        t_T_mm = T_pp * M1 * t_elt_mm
+        return nothing
+    end
+end
