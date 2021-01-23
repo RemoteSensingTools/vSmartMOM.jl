ea460154c52df5db69d32a02002f92854ece3ed5
cfranken <cfranken@tofu.gps.caltech.edu>
2021-01-23 11:10:58 -0800

    Add D matrix as kernel, something buggy

--- src/RTM/rt_doubling.jl
+++ src/RTM/rt_doubling.jl
@@ -2,12 +2,14 @@
 # Prototype doubling methods, compute homogenous layer matrices from its elemental layer in 
 # `ndoubl` doubling steps
 
-function rt_doubling_helper!(ndoubl::Int, added_layer::AddedLayer,
-                             D::AbstractArray{FT,3},
-                             I_static::AbstractArray) where {FT}
+function rt_doubling_helper!(pol_type,ndoubl::Int, 
+                            added_layer::AddedLayer,
+                            D::AbstractArray{FT,3},
+                            I_static::AbstractArray, 
+                            architecture) where {FT}
 
     @unpack r⁺⁻, r⁻⁺, t⁻⁻, t⁺⁺ = added_layer
-
+    dev = devi(architecture)
     # # ToDo: Important output doubling applied to elemental layer, using same variables 
     # r⁺⁻, r⁻⁺, t⁻⁻, t⁺⁺ (can be renamed to t⁺⁺, etc)
 
@@ -28,20 +30,74 @@ function rt_doubling_helper!(ndoubl::Int, added_layer::AddedLayer,
     end
 
     # After doubling, revert D(DR)->R, where D = Diagonal{1,1,-1,-1}
-    r⁻⁺[:] = D ⊠ r⁻⁺ ⊠ D
+    event = apply_D_matrix!(r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻)
+    wait(event)
+    # applyD_kernel! = apply_D!(dev)
+    # @show pol_type.n
+    # event = applyD_kernel!(r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻, ndrange=length(r⁻⁺));
+    # wait(event)
+    # synchronize()
+    @show r⁻⁺ ≈ r⁺⁻
+    @show t⁺⁺ ≈ t⁻⁻
+    # 
+    # r⁺⁻[:] = r⁻⁺
+    # t⁻⁻[:] = t⁺⁺
+    # After doubling, revert D(DR)->R, where D = Diagonal{1,1,-1,-1}
+    # r⁻⁺[:] = D ⊠ r⁻⁺ ⊠ D
 
     # Using r⁺⁻ = Dr⁻⁺D
-    r⁺⁻[:] = D ⊠ r⁻⁺ ⊠ D
+    # r⁺⁻[:] = D ⊠ r⁻⁺ ⊠ D
     
     # Using t⁻⁻ = Dt⁺⁺D
-    t⁻⁻[:] = D ⊠ t⁺⁺ ⊠ D
+    # t⁻⁻[:] = D ⊠ t⁺⁺ ⊠ D
 
     return nothing 
 end
 
-function rt_doubling!(ndoubl::Int, added_layer::AddedLayer,
-                      D::AbstractArray{FT,3}, I_static::AbstractArray) where {FT}
+function rt_doubling!(pol_type,ndoubl::Int, added_layer::AddedLayer,
+                    D::AbstractArray{FT,3},
+                    I_static::AbstractArray, 
+                    architecture) where {FT}
 
-    rt_doubling_helper!(ndoubl, added_layer, D, I_static)
+    rt_doubling_helper!(pol_type, ndoubl, added_layer, D, I_static, architecture)
     synchronize()
+end
+
+@kernel function apply_D2!(n_stokes::Int,  r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻)
+    iμ, jμ, n = @index(Global, NTuple)
+    if n_stokes == 1
+        r⁺⁻[iμ,jμ,n] = r⁻⁺[iμ,jμ,n]
+        t⁻⁻[iμ,jμ,n] = t⁺⁺[iμ,jμ,n]
+    else
+        i = mod(iμ - 1, n_stokes)
+        j = mod(jμ - 1, n_stokes)
+        # @show i,j
+        if (i >= 2)
+            r⁻⁺[iμ,jμ,n] = - r⁻⁺[iμ, jμ,n]
+        end
+        if ((i <= 1) & (j <= 1)) | ((i >= 2) & (j >= 2))
+            r⁺⁻[iμ,jμ,n] = r⁻⁺[iμ,jμ,n]
+            t⁻⁻[iμ,jμ,n] = t⁺⁺[iμ,jμ,n]
+        else
+            r⁺⁻[iμ,jμ,n] = - r⁻⁺[iμ,jμ,n]
+            t⁻⁻[iμ,jμ,n] = - t⁺⁺[iμ,jμ,n]
+        end
+    end
+    @synchronize()
+end
+
+@kernel function apply_D!(r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻)
+    i, j, n = @index(Global, NTuple)
+    r⁺⁻[i,j,n] = r⁻⁺[i,j,n]
+    t⁻⁻[i,j,n] = t⁺⁺[i,j,n]
+end
+
+function apply_D_matrix!(r⁻⁺::CuArray{FT,3}, t⁺⁺::CuArray{FT,3}, r⁺⁻::CuArray{FT,3}, t⁻⁻::CuArray{FT,3}) where {FT}
+    applyD_kernel! = apply_D!(KernelAbstractions.CUDADevice())
+    applyD_kernel!(r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻, ndrange=size(r⁻⁺));
+end
+
+function apply_D_matrix!(r⁻⁺::Array{FT,3}, t⁺⁺::Array{FT,3}, r⁺⁻::Array{FT,3}, t⁻⁻::Array{FT,3}) where {FT}
+    applyD_kernel! = apply_D!(KernelAbstractions.CPU())
+    applyD_kernel!(r⁻⁺, t⁺⁺, r⁺⁻, t⁻⁻, ndrange=size(r⁻⁺));
 end
