0c2ceb330eafc6730685c897fa386fd79a5318db
Christian Frankenberg <fronge@gmail.com>
2021-03-21 17:13:00 -0700

    Check Interaction still, matrix order might be wrong

--- src/vSmartMOM/Solvers/interaction.jl
+++ src/vSmartMOM/Solvers/interaction.jl
@@ -61,41 +61,35 @@ function interaction_helper!(::ScatteringInterface_11, SFI,
     @unpack r⁺⁻, r⁻⁺, t⁻⁻, t⁺⁺ = added_layer
     @unpack R⁻⁺, R⁺⁻, T⁺⁺, T⁻⁻, J₀⁺, J₀⁻ = composite_layer
 
-    # Used to store `(I - R⁺⁻ * r⁻⁺)⁻¹ * T⁺⁺`
+    # Used to store `(I - R⁺⁻ * r⁻⁺)⁻¹`
     tmp_inv = similar(t⁺⁺)
 
-    # Compute and store `(I - R⁺⁻ * r⁻⁺)⁻¹ * T⁺⁺`
+    # Compute and store `(I - R⁺⁻ * r⁻⁺)⁻¹`
     @timeit "interaction inv1" batch_inv!(tmp_inv, I_static .- R⁺⁻ ⊠ r⁻⁺) #Suniti
     # Temporary arrays:
-    # T₂₁(I-R₀₁R₂₁)⁻¹
-    T21_inv = T⁻⁻ ⊠ tmp_inv;
+    # T₁₂(I-R₀₁R₂₁)⁻¹
+    T12_inv = T⁻⁻ ⊠ tmp_inv;
     # T₀₁(I-R₀₁R₂₁)⁻¹
-    T01_inv = t⁺⁺ ⊠ tmp_inv 
+    T01_inv = t⁺⁺ ⊠ tmp_inv
+    # R₂₀ = R₁₀ + T₀₁(I-R₀₁R₂₁)⁻¹ R₂₁T₁₀ 
     composite_layer.R⁻⁺[:] = R⁻⁺ + T01_inv ⊠ r⁻⁺ ⊠ T⁺⁺ #Suniti
+    # T₂₀ = T₂₁(I-R₀₁R₂₁)⁻¹T₁₀
     composite_layer.T⁺⁺[:] = T01_inv  ⊠ T⁺⁺ #Suniti
-    #composite_layer.J₀⁺[:] = J₀⁺ + t⁺⁺ ⊠ tmp_inv ⊠ (R⁺⁻ ⊠ J₀⁻ + J₀⁺)
     if SFI
-        composite_layer.J₀⁺[:] = added_layer.J₀⁺ + T01_inv ⊠ (R⁺⁻ ⊠ added_layer.J₀⁻ + J₀⁺)
-        #for nλ=1:size(J₀⁺,3)     
-        #    composite_layer.J₀⁺[:,1,nλ] = added_layer.J₀⁺[:,1,nλ] + t⁺⁺[:,:,nλ] * tmp_inv[:,:,nλ] * (R⁺⁻[:,:,nλ] * added_layer.J₀⁻[:,1,nλ] + J₀⁺[:,1,nλ])
-        #end 
+        # J₂₀ = J₂₁ + T₂₁(I-R₀₁R₂₁)⁻¹(J₁₀ + R₀₁J₁₂)
+        composite_layer.J₀⁺[:] = added_layer.J₀⁺ + T01_inv ⊠ (J₀⁺ + R⁺⁻ ⊠ added_layer.J₀⁻)
     end 
     # Repeating for mirror-reflected directions
 
-    # Compute and store `(I - r⁻⁺ * R⁺⁻)⁻¹ * t⁻⁻`
+    # Compute and store `(I - r⁻⁺ * R⁺⁻)⁻¹`
     @timeit "interaction inv2" batch_inv!(tmp_inv, I_static .- r⁻⁺ ⊠ R⁺⁻) #Suniti
-
+    # R₀₂ = R₁₂ + T₂₁(I-R₀₁R₂₁)⁻¹ R₂₁T₁₀
     composite_layer.R⁺⁻[:] = r⁺⁻ + T01_inv ⊠ R⁺⁻ ⊠ t⁻⁻ #Suniti
     composite_layer.T⁻⁻[:] = T⁺⁺ ⊠ tmp_inv ⊠ t⁻⁻ #Suniti
 
     if SFI
         composite_layer.J₀⁻[:] = J₀⁻ + T21_inv ⊠ (r⁻⁺ ⊠ J₀⁻ + added_layer.J₀⁻)
-        #for nλ=1:size(J₀⁺,3)
-        #    composite_layer.J₀⁻[:,1,nλ] = J₀⁻[:,1,nλ] + T⁻⁻[:,:,nλ] * tmp_inv[:,:,nλ] * (r⁻⁺[:,:,nλ] * J₀⁺[:,1,nλ] + added_layer.J₀⁻[:,1,nλ])
-        #end
     end 
-
-    #@pack! composite_layer = R⁻⁺, R⁺⁻, T⁺⁺, T⁻⁻, J₀⁺, J₀⁻
 end
 
 
