5077d47d01e97fd5ff2ebb51c26bb6e5018adcbc
RupeshJey <rupeshjey@gmail.com>
2021-03-29 14:53:32 -0700

    Alter yaml structure

--- test/test_Scattering.jl
+++ test/test_Scattering.jl
@@ -71,6 +71,8 @@ end
 
     # STEP 1: Create the Aerosol
 
+    τ
+
     # Aerosol particle distribution and properties 
     μ  = 0.3                # Log mean radius
     σ  = 6.82               # Log stddev of radius
