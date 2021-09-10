ef8b6976bdc1eee06eaf962770dbeac440acb58c
Rupesh Jeyaram <rupeshjey@gmail.com>
2021-09-09 17:37:33 -0700

    Set up small RT tests

--- src/vSmartMOM/RT_proto_sequence.jl
+++ src/vSmartMOM/RT_proto_sequence.jl
@@ -14,11 +14,47 @@ using RadiativeTransfer.SolarModel
 # using InstrumentOperator
 # using ForwardDiff
 
-# Load parameters from file
-# parameters = vSmartMOM.default_parameters()
-parameters = vSmartMOM.parameters_from_yaml("RadiativeTransfer/test/helper/PureRayleighParameters.yaml");
-model = model_from_parameters(parameters);
-R_new4 = vSmartMOM.rt_run(model, i_band=1)
+R_0 = [0.03916, 0.04235, 0.04378, 0.04593, 0.04940, 0.05438, 0.05816, 0.06541, 0.07639, 0.08773, 0.09549, 0.11842, 0.13619, 0.17836, 0.22393, 0.27069]
+
+R_90 = [0.03916, 0.03922, 0.03930, 0.03952, 0.04021, 0.04208, 0.04409, 0.04894, 0.05748, 0.06698, 0.07367, 0.09388, 0.10977, 0.14790, 0.18964, 0.23441]
+
+R_180 = [0.03916, 0.03634, 0.03533, 0.03414, 0.03316, 0.03381, 0.03557, 0.04073, 0.05062, 0.06186, 0.06981, 0.09387, 0.11280, 0.15826, 0.20833, 0.26423]
+
+R_trues = [R_0, R_90, R_180]
+
+R_modeled = zeros(16, 3)
+
+for i in 1:3 #[0, 90, 180]
+
+    # Load parameters from file
+    # parameters = vSmartMOM.default_parameters()
+    parameters = vSmartMOM.parameters_from_yaml("/home/rjeyaram/RadiativeTransfer/test/helper/6SV1_1.yaml");
+
+    parameters.vaz = repeat([[0, 90, 180][i]], 16)
+
+    model = model_from_parameters(parameters);
+
+    model.τ_rayl[1] *= 0.1/sum(model.τ_rayl[1])
+    # model.τ_rayl[1] .= 0.1/length(model.τ_rayl[1])
+
+    # @assert sum(model.τ_rayl[1]) == 0.1
+
+    R_modeled[:,i] = vSmartMOM.rt_run(model, i_band=1)[:,:,1]
+
+end
+
+plot(R_modeled[:,1])
+plot!(R_trues[1])
+
+plot(R_modeled[:,2])
+plot!(R_trues[2])
+
+plot(R_modeled[:,3])
+plot!(R_trues[3])
+
+# [0.0000, 11.4783, 16.2602, 23.0739, 32.8599, 43.9455, 50.2082, 58.6677, 66.4218, 71.3371, 73.7398, 78.4630, 80.7931, 84.2608, 86.5602, 88.854]
+
+# "/home/rjeyaram/RadiativeTransfer/test/helper/6SV1_1.yaml"
 
 # params_dict = YAML.load_file("RadiativeTransfer/src/vSmartMOM/ModelParameters/DefaultParameters.yaml")
 
@@ -35,28 +71,28 @@ R_new4 = vSmartMOM.rt_run(model, i_band=1)
 
 #model.params.architecture = RadiativeTransfer.Architectures.GPU();
 
-function run_auto(x)
-    (x)
-    (x[1] isa ForwardDiff.Dual)
-    (x[1])
-    (typeof(parameters.μ))
-    parameters.μ  = [x[1]]
-    parameters.σ  = [x[2]]
-    parameters.nᵣ = [x[3]]
-    parameters.nᵢ = [x[4]]
-    model = model_from_parameters(parameters);
-    # model.architecture = RadiativeTransfer.Architectures.CPU()
-    model.params.architecture = RadiativeTransfer.Architectures.CPU()
-    R = vSmartMOM.rt_run(model, i_band=1)
-    return R[1,1,:]
-end
+# function run_auto(x)
+#     (x)
+#     (x[1] isa ForwardDiff.Dual)
+#     (x[1])
+#     (typeof(parameters.μ))
+#     parameters.μ  = [x[1]]
+#     parameters.σ  = [x[2]]
+#     parameters.nᵣ = [x[3]]
+#     parameters.nᵢ = [x[4]]
+#     model = model_from_parameters(parameters);
+#     # model.architecture = RadiativeTransfer.Architectures.CPU()
+#     model.params.architecture = RadiativeTransfer.Architectures.CPU()
+#     R = vSmartMOM.rt_run(model, i_band=1)
+#     return R[1,1,:]
+# end
 
-x = [1.3, 2.0, 1.3, 0.000001];
+# x = [1.3, 2.0, 1.3, 0.000001];
 
-# @time run_auto(x)
-@time dfdx = ForwardDiff.jacobian(run_auto, x);
+# # @time run_auto(x)
+# @time dfdx = ForwardDiff.jacobian(run_auto, x);
 
-a = 10
+# a = 10
 
 # g = x -> ForwardDiff.jacobian(run_auto, x);
 
@@ -101,3 +137,5 @@ a = 10
 # a[380:(end-1)] .= solar_interpolated
 
 # plot(1:1500, a .* Ls)
+
+# [0.03916, 0.04235, 0.04378, 0.04593, 0.04940, 0.05438, 0.05816, 0.06541, 0.07639, 0.08773, 0.09549, 0.11842, 0.13619, 0.17836, 0.22393, 0.27069]
