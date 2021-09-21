30ef0583182c862ef4fe8f66256538a861bb4996
Rupesh Jeyaram <rupeshjey@gmail.com>
2021-09-21 16:42:50 -0700

    Implement 6SV1 tests

--- test/test_vSmartMOM.jl
+++ test/test_vSmartMOM.jl
@@ -25,6 +25,43 @@
 
 end
 
+@testset "compare against 6SV1" begin 
+
+    include("6SV1_R_trues.jl")
+    R_modeled_all = zeros(6, 3, 3, 16);
+    R_deltas_all = zeros(6, 3, 3, 16);
+    parameters = parameters_from_yaml("6SV1_1.yaml");
+
+    function test_against_6SV1(case_i, azs, szas, λ, τ, ρ)
+        for sza_i in 1:length(szas)
+            for az_i in 1:length(azs)
+                R_true = R_trues[case_i]
+                R_modeled = view(R_modeled_all, case_i, :, :, :)
+                R_deltas = view(R_deltas_all, case_i, :, :, :)
+                parameters.spec_bands = [1e7/λ (1e7/λ + 1)]
+                parameters.vaz = repeat([azs[az_i]], 16)
+                parameters.sza = szas[sza_i]
+                parameters.brdf = [RadiativeTransfer.vSmartMOM.LambertianSurfaceScalar(ρ * π)]
+                model = model_from_parameters(parameters);
+                model.τ_rayl[1] .= τ
+                R_modeled[sza_i, az_i, :] = vSmartMOM.rt_run(model, i_band=1)[:,1,1] / model.quad_points.μ₀
+                R_deltas[sza_i, az_i, :] = abs.(R_true[sza_i][az_i] - R_modeled[sza_i, az_i, :]) ./ R_true[sza_i][az_i]
+            end
+        end
+        return maximum(R_deltas_all[case_i,:,:,:])
+    end
+
+    ϵ = 0.006
+
+    @test test_against_6SV1(1, [180, 90, 0], [23.0739, 53.1301, 78.4630], 530, 0.1, 0.0) < ϵ
+    @test test_against_6SV1(2, [180, 90, 0], [0.0001, 36.8699, 66.4218], 530, 0.1, 0.25) < ϵ
+    @test test_against_6SV1(3, [180, 90, 0], [0.0001, 36.8699, 66.4218], 440, 0.25, 0.0) < ϵ
+    @test test_against_6SV1(4, [180, 90, 0], [23.0739, 53.1301, 78.4630], 440, 0.25, 0.25) < ϵ
+    @test test_against_6SV1(5, [180, 90, 0], [23.0739, 53.1301, 78.4630], 360, 0.50, 0.0) < ϵ
+    @test test_against_6SV1(6, [180, 90, 0], [0.0001, 36.8699, 66.4218], 360, 0.50, 0.25) < ϵ
+
+end
+
 # # Test the O2 case
 # @testset "O2 RT" begin
 
