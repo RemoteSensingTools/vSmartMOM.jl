3494377a837bac761eecc9ecf9e34dfd1ef32d0b
cfranken <fronge@gmail.com>
2021-10-15 05:15:16 -0700

    Implement Spectrally Variable Lambertian Albedo

--- src/vSmartMOM/lambertian_surface.jl
+++ src/vSmartMOM/lambertian_surface.jl
@@ -34,31 +34,98 @@ function create_surface_layer!(lambertian::LambertianSurfaceScalar{FT},
         # Get size of added layer
         dim = size(added_layer.r⁻⁺)
         Nquad = dim[1] ÷ pol_type.n
-        # Ensure matrices are zero:
-        tmp = zeros(Real, pol_type.n)
-        tmp[1] = ρ   
-        R_surf = Array(Diagonal(vcat(ρ, zeros(pol_type.n-1))))
+        
+        R_surf = Array(Diagonal(vcat(ρ, zeros(FT,pol_type.n-1))))
         R_surf = repeat(R_surf',Nquad)
         R_surf = repeat(R_surf',Nquad)
+        
         # Move to architecture:
         R_surf = arr_type(R_surf)
+        
+        # Source function of surface:
         if SFI
             I₀_NquadN = similar(qp_μN);
             I₀_NquadN[:] .=0;
-            i_start   = pol_type.n*(iμ₀-1) + 1 
-            i_end     = pol_type.n*iμ₀;
-            I₀_NquadN[i_start:i_end] = pol_type.I₀;
+            #i_start   = pol_type.n*(iμ₀-1) + 1 
+            #@show i_start, iμ₀Nstart
+            #i_end     = pol_type.n*iμ₀;
+            I₀_NquadN[iμ₀Nstart:pol_type.n*iμ₀] = pol_type.I₀;
         
             added_layer.J₀⁺[:] .= 0
-            added_layer.J₀⁻[:,1,:] = μ₀*(R_surf*I₀_NquadN) .* exp.(-τ_sum/μ₀)';  
+            added_layer.J₀⁻[:,1,:] = μ₀*(R_surf*I₀_NquadN) .* exp.(-τ_sum/μ₀)';
+            #@show μ₀*(1.1*R_surf*I₀_NquadN) == μ₀*(R_surf*I₀_NquadN)*1.1  
             #repeat(repeat(tmp .* exp.(-τ_sum/qp_μN[i_start]), Nquad), 1,1,dim[3])
         end
         R_surf = R_surf * Diagonal(qp_μN.*wt_μN)
         tmp    = ones(pol_type.n*Nquad)
         T_surf = arr_type(Diagonal(tmp))
 
-        
+        @show size(added_layer.r⁻⁺), size(R_surf), size(added_layer.J₀⁻)
         added_layer.r⁻⁺ .= R_surf;
+        added_layer.r⁺⁻ .= 0;
+        added_layer.t⁺⁺ .= T_surf;
+        added_layer.t⁻⁻ .= T_surf;
+
+    else
+        added_layer.r⁻⁺[:] .= 0;
+        added_layer.r⁻⁺[:] .= 0;
+        added_layer.t⁺⁺[:] .= 0;
+        added_layer.t⁻⁻[:] .= 0;
+        added_layer.J₀⁺[:] .= 0;
+        added_layer.J₀⁻[:] .= 0;
+    end
+end
+
+function create_surface_layer!(lambertian::LambertianSurfaceLegendre{FT}, 
+    added_layer::AddedLayer,
+    SFI,
+    m::Int,
+    pol_type,
+    quad_points,
+    τ_sum,
+    architecture) where {FT}
+
+    if m == 0
+        @unpack qp_μ, wt_μ, qp_μN, wt_μN, iμ₀Nstart, iμ₀, μ₀ = quad_points
+        legendre_coeff = lambertian.legendre_coeff
+        arr_type = array_type(architecture)
+        # Albedo normalized by π (and factor 2 for 0th Fourier Moment)
+        # a) Define range for legendre polymonial:
+        x = collect(range(FT(-1), FT(1), length=length(τ_sum)));
+        # Legendre Polynomial basis functions:
+        P = Scattering.compute_legendre_poly(x,length(legendre_coeff))[1]
+        # Evaluate Polynomial (as matrix multiplication)
+        albedo = P * legendre_coeff
+        @show albedo
+        ρ = 2albedo/FT(π)
+        # Get size of added layer
+        dim = size(added_layer.r⁻⁺)
+        Nquad = dim[1] ÷ pol_type.n
+
+        R_surf = Array(Diagonal(vcat(FT(1), zeros(FT,pol_type.n-1))))
+        R_surf = repeat(R_surf',Nquad)
+        R_surf = repeat(R_surf',Nquad)
+
+        # Move to architecture:
+        R_surf = arr_type(R_surf)
+
+        # Source function of surface:
+        if SFI
+            I₀_NquadN = similar(qp_μN);
+            I₀_NquadN[:] .=0;
+            I₀_NquadN[iμ₀Nstart:pol_type.n*iμ₀] = pol_type.I₀;
+            added_layer.J₀⁺[:] .= 0
+            # Suniti double-check
+            added_layer.J₀⁻[:,1,:] = μ₀*(R_surf*I₀_NquadN) .* (ρ .* exp.(-τ_sum/μ₀))';
+        end
+        R_surf   = R_surf * Diagonal(qp_μN.*wt_μN)
+        siz = size(added_layer.r⁻⁺)
+        R_surf3D = reshape(reduce(hcat,[i*R_surf for i in ρ]), siz...);
+        tmp    = ones(pol_type.n*Nquad)
+        T_surf = arr_type(Diagonal(tmp))
+
+        @show size(added_layer.r⁻⁺), size(R_surf), size(added_layer.J₀⁻)
+        added_layer.r⁻⁺ .= R_surf3D;
         added_layer.r⁺⁻ .= 0;
         added_layer.t⁺⁺ .= T_surf;
         added_layer.t⁻⁻ .= T_surf;
