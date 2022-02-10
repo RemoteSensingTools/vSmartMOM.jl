66f5b3c136431277fe8aaec57224d31cfbd10457
sunitisanghavi <suniti.sanghavi@gmail.com>
2022-02-10 12:24:50 -0800

    working proto VRS

--- src/vSmartMOM/model_from_parameters.jl
+++ src/vSmartMOM/model_from_parameters.jl
@@ -48,7 +48,9 @@ function model_from_parameters(params::vSmartMOM_Parameters)
         curr_band_λ = 1e4 ./ params.spec_bands[i_band]
 
         # Compute Rayleigh properties per layer for `i_band` band center 
-        τ_rayl[i_band]   = getRayleighLayerOptProp(profile.p_half[end], (maximum(curr_band_λ) + minimum(curr_band_λ)/2), params.depol, profile.vcd_dry);
+        τ_rayl[i_band]   = getRayleighLayerOptProp(profile.p_half[end], 
+                                (maximum(curr_band_λ) + minimum(curr_band_λ)/2), 
+                                params.depol, profile.vcd_dry);
 
         # If no absorption, continue to next band
         isnothing(params.absorption_params) && continue
@@ -57,17 +59,31 @@ function model_from_parameters(params::vSmartMOM_Parameters)
         for molec_i in 1:length(params.absorption_params.molecules[i_band])
 
             # Obtain hitran data for this molecule
-            @timeit "Read HITRAN"  hitran_data = read_hitran(artifact(params.absorption_params.molecules[i_band][molec_i]), iso=1)
+            @timeit "Read HITRAN"  hitran_data = read_hitran(
+                        artifact(params.absorption_params.molecules[i_band][molec_i]), 
+                        iso=1)
 
-            println("Computing profile for $(params.absorption_params.molecules[i_band][molec_i]) with vmr $(profile.vmr[params.absorption_params.molecules[i_band][molec_i]]) for band #$(i_band)")
+            println("Computing profile for 
+                $(params.absorption_params.molecules[i_band][molec_i]) 
+                with vmr 
+                $(profile.vmr[params.absorption_params.molecules[i_band][molec_i]]) 
+                for band #$(i_band)")
 
             # Calculate absorption profile
-            @timeit "Absorption Coeff"  compute_absorption_profile!(τ_abs[i_band], hitran_data, params.absorption_params.broadening_function, params.absorption_params.wing_cutoff, params.absorption_params.CEF, params.architecture, profile.vmr[params.absorption_params.molecules[i_band][molec_i]], params.spec_bands[i_band], profile);
+            @timeit "Absorption Coeff"  compute_absorption_profile!(τ_abs[i_band], 
+                                            hitran_data, 
+                                            params.absorption_params.broadening_function, 
+                                            params.absorption_params.wing_cutoff, 
+                                            params.absorption_params.CEF, 
+                                            params.architecture, 
+                                            profile.vmr[params.absorption_params.molecules[i_band][molec_i]], 
+                                            params.spec_bands[i_band], 
+                                            profile);
         end
     end
 
     # aerosol_optics[iBand][iAer]
-    aerosol_optics = [Array{AerosolOptics}(undef, (n_aer)) for i=1:length(0)];
+    aerosol_optics = [Array{AerosolOptics}(undef, (n_aer)) for i=1:n_bands];
         
     FT2 = isnothing(params.scattering_params) ? params.float_type : typeof(params.scattering_params.rt_aerosols[1].τ_ref)
     FT2 =  params.float_type 
@@ -91,10 +107,16 @@ function model_from_parameters(params::vSmartMOM_Parameters)
         #mie_aerosol = make_mie_aerosol(size_distribution, curr_aerosol.nᵣ, curr_aerosol.nᵢ, params.scattering_params.r_max, params.scattering_params.nquad_radius) #Suniti: why is the refractive index needed here?
 
         # Create the aerosol extinction cross-section at the reference wavelength:
-        mie_model      = make_mie_model(params.scattering_params.decomp_type, mie_aerosol, params.scattering_params.λ_ref, params.polarization_type, truncation_type, params.scattering_params.r_max, params.scattering_params.nquad_radius)       
+        mie_model      = make_mie_model(params.scattering_params.decomp_type, 
+                                        mie_aerosol, 
+                                        params.scattering_params.λ_ref, 
+                                        params.polarization_type, 
+                                        truncation_type, 
+                                        params.scattering_params.r_max, 
+                                        params.scattering_params.nquad_radius)       
         k_ref          = compute_ref_aerosol_extinction(mie_model, params.float_type)
 
-        params.scattering_params.rt_aerosols[i_aer].p₀, params.scattering_params.rt_aerosols[i_aer].σp
+        #params.scattering_params.rt_aerosols[i_aer].p₀, params.scattering_params.rt_aerosols[i_aer].σp
         # Loop over bands
         for i_band=1:n_bands
             
@@ -102,7 +124,13 @@ function model_from_parameters(params::vSmartMOM_Parameters)
             curr_band_λ = 1e4 ./ params.spec_bands[i_band]
 
             # Create the aerosols:
-            mie_model      = make_mie_model(params.scattering_params.decomp_type, mie_aerosol, (maximum(curr_band_λ)+minimum(curr_band_λ))/2, params.polarization_type, truncation_type, params.scattering_params.r_max, params.scattering_params.nquad_radius)
+            mie_model      = make_mie_model(params.scattering_params.decomp_type, 
+                                            mie_aerosol, 
+                                            (maximum(curr_band_λ)+minimum(curr_band_λ))/2, 
+                                            params.polarization_type, 
+                                            truncation_type, 
+                                            params.scattering_params.r_max, 
+                                            params.scattering_params.nquad_radius)
 
             # Compute raw (not truncated) aerosol optical properties (not needed in RT eventually) 
             # @show FT2
@@ -110,17 +138,205 @@ function model_from_parameters(params::vSmartMOM_Parameters)
 
             # Compute truncated aerosol optical properties (phase function and fᵗ), consistent with Ltrunc:
             @show i_aer, i_band
-            aerosol_optics[i_band][i_aer] = Scattering.truncate_phase(truncation_type, aerosol_optics_raw; reportFit=false)
+            aerosol_optics[i_band][i_aer] = Scattering.truncate_phase(truncation_type, 
+                                                    aerosol_optics_raw; reportFit=false)
 
             # Compute nAer aerosol optical thickness profiles
-            τ_aer[i_band][i_aer,:] = params.scattering_params.rt_aerosols[i_aer].τ_ref[] * (aerosol_optics[i_band][i_aer].k/k_ref) * vSmartMOM.getAerosolLayerOptProp(1.0, c_aero.p₀, c_aero.σp, profile.p_full)
-            
+            τ_aer[i_band][i_aer,:] = 
+                params.scattering_params.rt_aerosols[i_aer].τ_ref[] * 
+                (aerosol_optics[i_band][i_aer].k/k_ref) * 
+                vSmartMOM.getAerosolLayerOptProp(1.0, c_aero.p₀, c_aero.σp, profile.p_full)
         end 
     end
 
     # Check the floating-type output matches specified FT
 
     # Return the model 
-    return vSmartMOM_Model(params, aerosol_optics,  greek_rayleigh, quad_points, τ_abs, τ_rayl, τ_aer, obs_geom, profile)
+    return vSmartMOM_Model(params, 
+                        aerosol_optics,  
+                        greek_rayleigh, 
+                        quad_points, 
+                        τ_abs, 
+                        τ_rayl, 
+                        τ_aer, 
+                        obs_geom, 
+                        profile)
+end
 
+
+#=
+
+Modified version for vibrational Ramnan scattering
+
+=#
+
+
+"Take the parameters specified in the vSmartMOM_Parameters struct, and calculate derived attributes into a vSmartMOM_Model" 
+function model_from_parameters(RS_type::Union{VS_0to1_plus, VS_1to0_plus}, 
+                    λ₀,
+                    params::vSmartMOM_Parameters)
+    @show params.absorption_params.molecules
+    # Number of total bands and aerosols (for convenience)
+    n_bands = 3 #length(params.spec_bands)
+    n_aer = isnothing(params.scattering_params) ? 0 : length(params.scattering_params.rt_aerosols)
+
+    # Create observation geometry
+    obs_geom = vSmartMOM.ObsGeometry(params.sza, params.vza, params.vaz, params.obs_alt)
+
+    # Create truncation type
+    truncation_type = vSmartMOM.Scattering.δBGE{params.float_type}(params.l_trunc, params.Δ_angle)
+
+    # Set quadrature points for streams
+    quad_points = vSmartMOM.rt_set_streams(params.quadrature_type, params.l_trunc, obs_geom, params.polarization_type, array_type(params.architecture))
+
+    # Get AtmosphericProfile from parameters
+    vmr = isnothing(params.absorption_params) ? Dict() : params.absorption_params.vmr
+    p_full, p_half, vmr_h2o, vcd_dry, vcd_h2o, new_vmr = vSmartMOM.compute_atmos_profile_fields(params.T, params.p, params.q, vmr)
+    profile = vSmartMOM.AtmosphericProfile(params.T, p_full, params.q, p_half, vmr_h2o, vcd_dry, vcd_h2o, new_vmr)
+    
+    # Reduce the profile to the number of target layers (if specified)
+    if params.profile_reduction_n != -1
+        profile = vSmartMOM.reduce_profile(params.profile_reduction_n, profile);
+    end
+
+    effT = (profile.vcd_dry' * profile.T) / sum(profile.vcd_dry);
+    # Define RS type
+    # Compute N2 and O2
+    RS_type.n2, RS_type.o2 = 
+        InelasticScattering.getRamanAtmoConstants(1.e7/λ₀,effT);
+    println("here 0")
+    InelasticScattering.getRamanSSProp!(RS_type, λ₀);
+    println("here 1")
+    n_bands = length(RS_type.iBand)
+    @show RS_type.grid_in
+    params.spec_bands = RS_type.grid_in
+    @show params.spec_bands
+
+    # Rayleigh optical properties calculation
+    greek_rayleigh = Scattering.get_greek_rayleigh(params.depol)
+    τ_rayl = [zeros(params.float_type, length(params.T)) for i=1:n_bands];
+
+    # τ_abs[iBand][iSpec,iZ]
+    τ_abs     = [zeros(params.float_type, length(params.spec_bands[i]), length(profile.p_full)) for i in 1:n_bands]
+    @show params.absorption_params.molecules[2]
+    # Loop over all bands:
+    for i_band=1:n_bands
+        @show params.spec_bands[i_band]
+        # i'th spectral band (convert from cm⁻¹ to μm)
+        curr_band_λ = 1e4 ./ params.spec_bands[i_band]
+
+        # Compute Rayleigh properties per layer for `i_band` band center 
+        τ_rayl[i_band]   = getRayleighLayerOptProp(profile.p_half[end], 
+                                (maximum(curr_band_λ) + minimum(curr_band_λ)/2), 
+                                params.depol, profile.vcd_dry);
+
+        # If no absorption, continue to next band
+        isnothing(params.absorption_params) && continue
+        @show i_band, params.absorption_params.molecules[i_band]
+        # Loop over all molecules in this band, obtain profile for each, and add them up
+        for molec_i in 1:length(params.absorption_params.molecules[i_band])
+
+            # Obtain hitran data for this molecule
+            @timeit "Read HITRAN"  hitran_data = read_hitran(
+                        artifact(params.absorption_params.molecules[i_band][molec_i]), 
+                        iso=1)
+
+            println("Computing profile for 
+                $(params.absorption_params.molecules[i_band][molec_i]) 
+                with vmr 
+                $(profile.vmr[params.absorption_params.molecules[i_band][molec_i]]) 
+                for band #$(i_band)")
+
+            # Calculate absorption profile
+            @timeit "Absorption Coeff"  compute_absorption_profile!(τ_abs[i_band], 
+                                            hitran_data, 
+                                            params.absorption_params.broadening_function, 
+                                            params.absorption_params.wing_cutoff, 
+                                            params.absorption_params.CEF, 
+                                            params.architecture, 
+                                            profile.vmr[params.absorption_params.molecules[i_band][molec_i]], 
+                                            params.spec_bands[i_band], 
+                                            profile);
+        end
+    end
+
+    # aerosol_optics[iBand][iAer]
+    aerosol_optics = [Array{AerosolOptics}(undef, (n_aer)) for i=1:n_bands];
+        
+    FT2 = isnothing(params.scattering_params) ? params.float_type : typeof(params.scattering_params.rt_aerosols[1].τ_ref)
+    FT2 =  params.float_type 
+
+    # τ_aer[iBand][iAer,iZ]
+    τ_aer = [zeros(FT2, n_aer, length(profile.p_full)) for i=1:n_bands];
+
+    # Loop over aerosol type
+    for i_aer=1:n_aer
+
+        # Get curr_aerosol
+        c_aero = params.scattering_params.rt_aerosols[i_aer]
+        curr_aerosol = c_aero.aerosol
+        
+        # Create Aerosol size distribution for each aerosol species
+        size_distribution = curr_aerosol.size_distribution
+
+        # Create a univariate aerosol distribution
+        mie_aerosol = Aerosol(size_distribution, curr_aerosol.nᵣ, curr_aerosol.nᵢ)
+        @show typeof(curr_aerosol.nᵣ)
+        #mie_aerosol = make_mie_aerosol(size_distribution, curr_aerosol.nᵣ, curr_aerosol.nᵢ, params.scattering_params.r_max, params.scattering_params.nquad_radius) #Suniti: why is the refractive index needed here?
+
+        # Create the aerosol extinction cross-section at the reference wavelength:
+        mie_model      = make_mie_model(params.scattering_params.decomp_type, 
+                                        mie_aerosol, 
+                                        params.scattering_params.λ_ref, 
+                                        params.polarization_type, 
+                                        truncation_type, 
+                                        params.scattering_params.r_max, 
+                                        params.scattering_params.nquad_radius)       
+        k_ref          = compute_ref_aerosol_extinction(mie_model, params.float_type)
+
+        #params.scattering_params.rt_aerosols[i_aer].p₀, params.scattering_params.rt_aerosols[i_aer].σp
+        # Loop over bands
+        for i_band=1:n_bands
+            
+            # i'th spectral band (convert from cm⁻¹ to μm)
+            curr_band_λ = 1e4 ./ params.spec_bands[i_band]
+
+            # Create the aerosols:
+            mie_model      = make_mie_model(params.scattering_params.decomp_type, 
+                                            mie_aerosol, 
+                                            (maximum(curr_band_λ)+minimum(curr_band_λ))/2, 
+                                            params.polarization_type, 
+                                            truncation_type, 
+                                            params.scattering_params.r_max, 
+                                            params.scattering_params.nquad_radius)
+
+            # Compute raw (not truncated) aerosol optical properties (not needed in RT eventually) 
+            # @show FT2
+            @timeit "Mie calc"  aerosol_optics_raw = compute_aerosol_optical_properties(mie_model, FT2);
+
+            # Compute truncated aerosol optical properties (phase function and fᵗ), consistent with Ltrunc:
+            @show i_aer, i_band
+            aerosol_optics[i_band][i_aer] = Scattering.truncate_phase(truncation_type, 
+                                                    aerosol_optics_raw; reportFit=false)
+
+            # Compute nAer aerosol optical thickness profiles
+            τ_aer[i_band][i_aer,:] = 
+                params.scattering_params.rt_aerosols[i_aer].τ_ref[] * 
+                (aerosol_optics[i_band][i_aer].k/k_ref) * 
+                vSmartMOM.getAerosolLayerOptProp(1.0, c_aero.p₀, c_aero.σp, profile.p_full)
+        end 
+    end
+
+    # Check the floating-type output matches specified FT
+
+    # Return the model 
+    return vSmartMOM_Model(params, 
+                        aerosol_optics,  
+                        greek_rayleigh, 
+                        quad_points, 
+                        τ_abs, 
+                        τ_rayl, 
+                        τ_aer, 
+                        obs_geom, 
+                        profile)
 end
--- src/vSmartMOM/rt_helper_functions.jl
+++ src/vSmartMOM/rt_helper_functions.jl
@@ -88,7 +88,7 @@ default_J_matrix_rand(FT, arr_type, dims, nSpec) = arr_type(randn(FT, tuple(dims
 
 
 "Make an added layer, supplying all default matrices"
-make_added_layer(RS_type::noRS, FT, arr_type, dims, nSpec) = AddedLayer(
+make_added_layer(RS_type::Union{noRS, noRS_plus}, FT, arr_type, dims, nSpec) = AddedLayer(
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec),
@@ -98,7 +98,7 @@ make_added_layer(RS_type::noRS, FT, arr_type, dims, nSpec) = AddedLayer(
                                                         )
 
 "Make an added layer, supplying all default matrices"
-make_added_layer(RS_type::RRS, FT, arr_type, dims, nSpec)  = AddedLayerRS(
+make_added_layer(RS_type::Union{RRS, RRS_plus,VS_0to1_plus, VS_1to0_plus}, FT, arr_type, dims, nSpec)  = AddedLayerRS(
                                                 default_matrix(FT, arr_type, dims, nSpec), 
                                                 default_matrix(FT, arr_type, dims, nSpec), 
                                                 default_matrix(FT, arr_type, dims, nSpec),
@@ -113,9 +113,9 @@ make_added_layer(RS_type::RRS, FT, arr_type, dims, nSpec)  = AddedLayerRS(
                                                 default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman)
                                                 )
                                                          
-"Make an added layer, supplying all default matrices"
+#="Make an added layer, supplying all default matrices"
 #Currently RS_type.n_Raman = 1
-make_added_layer(RS_type::Union{VS_0to1, VS_1to0}, FT, arr_type, dims, nSpec)  = AddedLayerRS(
+make_added_layer(RS_type::Union{VS_0to1_plus, VS_1to0_plus}, FT, arr_type, dims, nSpec)  = AddedLayerRS(
                                                 default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
                                                 default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
                                                 default_matrix(FT, arr_type, dims, RS_type.n_Raman),
@@ -135,6 +135,7 @@ make_added_layer(RS_type::Union{VS_0to1, VS_1to0}, FT, arr_type, dims, nSpec)  =
                                                 default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman),
                                                 default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman)
                                                 )
+                                                =#
 "Make an added layer, supplying all default matrices"
 #Currently RS_type.n_Raman = 1
 #=
@@ -160,7 +161,7 @@ make_added_layer(RS_type::Union{RVRS_0to1, RVRS_1to0}, FT, arr_type, dims, nSpec
                                                 )
 =#
 "Make a random added layer, supplying all random matrices"
-make_added_layer_rand(RS_type::noRS, FT, arr_type, dims, nSpec)  = AddedLayer(
+make_added_layer_rand(RS_type::Union{noRS, noRS_plus}, FT, arr_type, dims, nSpec)  = AddedLayer(
                                                         default_matrix_rand(FT, arr_type, dims, nSpec), 
                                                         default_matrix_rand(FT, arr_type, dims, nSpec), 
                                                         default_matrix_rand(FT, arr_type, dims, nSpec),
@@ -170,7 +171,7 @@ make_added_layer_rand(RS_type::noRS, FT, arr_type, dims, nSpec)  = AddedLayer(
                                                         )
                                                          
 "Make a composite layer, supplying all default matrices"
-make_composite_layer(RS_type::noRS,FT, arr_type, dims, nSpec) = CompositeLayer(
+make_composite_layer(RS_type::Union{noRS, noRS_plus}, FT, arr_type, dims, nSpec) = CompositeLayer(
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec),
@@ -179,7 +180,7 @@ make_composite_layer(RS_type::noRS,FT, arr_type, dims, nSpec) = CompositeLayer(
                                                         default_J_matrix(FT, arr_type, dims, nSpec)
                                                         )
 "Make a composite layer, supplying all default matrices"
-make_composite_layer(RS_type::RRS, FT, arr_type, dims, nSpec) = CompositeLayerRS(
+make_composite_layer(RS_type::Union{RRS, RRS_plus,VS_0to1_plus, VS_1to0_plus}, FT, arr_type, dims, nSpec) = CompositeLayerRS(
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec), 
                                                         default_matrix(FT, arr_type, dims, nSpec),
@@ -193,32 +194,9 @@ make_composite_layer(RS_type::RRS, FT, arr_type, dims, nSpec) = CompositeLayerRS
                                                         default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman),
                                                         default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman)
                                                         )
-
-"Make a composite layer, supplying all default matrices"
-make_composite_layer(RS_type::Union{VS_0to1, VS_1to0},FT, arr_type, dims, nSpec) = CompositeLayerRS(
-                                                        default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
-                                                        default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
-                                                        default_matrix(FT, arr_type, dims, RS_type.n_Raman),
-                                                        default_matrix(FT, arr_type, dims, RS_type.n_Raman),
-                                                        default_J_matrix(FT, arr_type, dims, RS_type.n_Raman),
-                                                        default_J_matrix(FT, arr_type, dims, RS_type.n_Raman),
-                                                        default_matrix(FT, arr_type, dims, nSpec), 
-                                                        default_matrix(FT, arr_type, dims, nSpec), 
-                                                        default_matrix(FT, arr_type, dims, nSpec),
-                                                        default_matrix(FT, arr_type, dims, nSpec),
-                                                        default_J_matrix(FT, arr_type, dims, nSpec),
-                                                        default_J_matrix(FT, arr_type, dims, nSpec),
-                                                        default_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman), 
-                                                        default_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman), 
-                                                        default_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman),
-                                                        default_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman),
-                                                        default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman),
-                                                        default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman)
-                                                        )
-
-"Make a composite layer, supplying all default matrices"
 #=
-make_composite_layer(RS_type::Union{RVRS_0to1, RVRS_1to0},FT, arr_type, dims, nSpec) = CompositeLayerRS(
+"Make a composite layer, supplying all default matrices"
+make_composite_layer(RS_type::Union{VS_0to1_plus, VS_1to0_plus},FT, arr_type, dims, nSpec) = CompositeLayerRS(
                                                         default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
                                                         default_matrix(FT, arr_type, dims, RS_type.n_Raman), 
                                                         default_matrix(FT, arr_type, dims, RS_type.n_Raman),
@@ -239,6 +217,7 @@ make_composite_layer(RS_type::Union{RVRS_0to1, RVRS_1to0},FT, arr_type, dims, nS
                                                         default_J_matrix_ie(FT, arr_type, dims, nSpec, RS_type.n_Raman)
                                                         )
 =#
+
                                                         
 
 "Given a ComputedAtmosphereProperties object, extract a ComputedLayerProperties object using data from the iz index of all arrays in the ComputedAtmosphereProperties"
