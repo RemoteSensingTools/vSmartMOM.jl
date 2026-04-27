4d1cd531375868ba7d4dc2cdd8b8c8ab11b3bf99
sunitisanghavi <suniti.sanghavi@gmail.com>
2026-04-27 11:46:29 -0700

    plot MODTRAN comparisons

--- test/benchmarks/plot_MODTRAN_vSmartMOM_4EMIT.jl
+++ test/benchmarks/plot_MODTRAN_vSmartMOM_4EMIT.jl
@@ -1,5 +1,6 @@
 using Plots
 using DelimitedFiles
+using LaTeXStrings
 
 function fmt(x, n)
     s = string(round(x; digits=n))
@@ -31,3 +32,38 @@ path2 = joinpath(MOM_DIR, filename2(h2o, aot, gndalt, tsz))
 # leave the tokenised column-name line in data2 and splay sphalb into a phantom
 # 9th column of empty strings.
 data2 = readdlm(path2, comments=true, comment_char='#')
+
+ylabels = [L"$\rho_\mathrm{atm}$", L"$T^\downarrow_\mathrm{dir}$", L"$T^\downarrow_\mathrm{dif}$", L"$T^\uparrow_\mathrm{dir}$", L"$T^\uparrow_\mathrm{dif}$", L"$S_\mathrm{atm}$"]
+xlabel = L"Wavelength [nm]"
+for i=3:8
+    plot(data1[:,1], data1[:,i], label="MODTRAN")#, xaxis=:log, yaxis=:log)
+    plot!(data2[:,1], data2[:,i], label="vSmartMOM")#, xaxis=:log, yaxis=:log)
+    xlabel!(xlabel)
+    ylabel!(ylabels[i-2])
+    savefig("MODTRAN_vSmartMOM_4EMIT_$(i-2).pdf")
+end
+
+vza = 12.0
+sza = 39.583965990615404
+ylabels = [L"$\tau_\mathrm{atm}$ [downward]", L"$\tau_\mathrm{atm}$ [upward]"]
+for i in [4,6]
+    if i==4
+        plot(-log.(data1[:,1])*cosd(sza), data1[:,i], label="MODTRAN")#, xaxis=:log, yaxis=:log)
+        plot!(-log.(data2[:,1])*cosd(sza), data2[:,i], label="vSmartMOM")#, xaxis=:log, yaxis=:log)
+        ylabel!(ylabels[1])
+        xlabel!(xlabel)
+        savefig("MODTRAN_vSmartMOM_4EMIT_tau_down.pdf")
+    else
+        plot(-log.(data1[:,1])*cosd(vza), data1[:,i], label="MODTRAN")#, xaxis=:log, yaxis=:log)
+        plot!(-log.(data2[:,1])*cosd(vza), data2[:,i], label="vSmartMOM")#, xaxis=:log, yaxis=:log)
+        ylabel!(ylabels[2])
+        xlabel!(xlabel)
+        savefig("MODTRAN_vSmartMOM_4EMIT_tau_up.pdf")
+    end
+end
+    
+plot(data1[:,1], -log.(data1[:,4])*cosd(sza) + log.(data1[:,6])*cosd(vza), label="MODTRAN")#, xaxis=:log, yaxis=:log)
+plot!(data2[:,1], -log.(data2[:,4])*cosd(sza) + log.(data2[:,6])*cosd(vza), label="vSmartMOM")#, xaxis=:log, yaxis=:log)
+ylabel!(L"$\Delta\tau_\mathrm{atm}$ [down-up]")
+xlabel!(xlabel)
+savefig("MODTRAN_vSmartMOM_4EMIT_tau_diff.pdf")
