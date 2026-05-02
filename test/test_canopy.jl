1ed180c1b59906144a56d44597105dd66c4c28da
cfranken <fronge@gmail.com>
2026-05-02 13:19:56 -0700

    test(canopy): restore specular composite Z check

--- test/test_canopy.jl
+++ test/test_canopy.jl
@@ -121,10 +121,11 @@ end
         @test all(iszero, Zmp_d[si:pol.n:end, sj:pol.n:end, :])
     end
 
-    specular_canopy = CanopySurface(; soil, LAI=1.0, canopy_scattering=specular,
+    composite = diffuse + specular
+    specular_canopy = CanopySurface(; soil, LAI=1.0, canopy_scattering=composite,
                                      canopy_quadrature=quadrature)
     Zpp_c, Zmp_c = CoreRT._compute_canopy_Z_stack(
-        specular_canopy, specular, μ, 2, Float64, pol.n)
+        specular_canopy, composite, μ, 2, Float64, pol.n)
 
     @test size(Zpp_c) == size(Zpp_d)
     @test all(isfinite, Zpp_c)
